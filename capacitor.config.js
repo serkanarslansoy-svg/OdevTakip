708cccba97ad3e85c79a7b74c29f7475ddee0adc
serkanarslansoy-svg <serkanarslansoy@gmail.com>
2026-09-25 09:35:11 +0200

    Prepare Android APK wrapper

---
+++ capacitor.config.js
@@ -0,0 +1,12 @@
+const config = {
+  appId: 'com.serkanarslansoy.odevavcisi',
+  appName: 'Ödev Avcısı',
+  webDir: 'www',
+  server: {
+    // Site uzaktan açılır; HTML güncellemeleri APK'yı yeniden kurmadan görünür.
+    url: 'https://serkanarslansoy-svg.github.io/OdevTakip/',
+    cleartext: false
+  }
+};
+
+module.exports = config;
